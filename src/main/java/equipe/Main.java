9250410659b373c6d994a5efc15310868ec0e63e
Juan Pablo <juanpabloflgalvao@gmail.com>
2026-09-23 20:13:03 -0300

    chore: cria e preenche a estrutura inicial do projeto

--- src/main/java/equipe/Main.java
+++ src/main/java/equipe/Main.java
@@ -1,4 +1,12 @@
 
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        Calc c = new Calc();
+
+        System.out.println("2 + 3 = " + c.soma(2, 3));
+        System.out.println("3 - 2 = " + c.subtracao(3, 2));
+        System.out.println("2 * 3 = " + c.multiplicacao(2, 3));
+    }
 }
